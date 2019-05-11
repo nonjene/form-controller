1f1d2e27040c8e9955d58c2b7412614789a7b2d2
nonjene <reanfn89@gmail.com>
2019-05-11 17:39:25 +0800

    ⏫ update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -12,55 +12,59 @@ const ip = require('ip');
 const port = 3000;
 
 const config = {
-  devtool: '#eval-source-map',
-  entry: {
-    app: './demo/index.js',
-  },
-  output: {
-    path: path.join(__dirname, './build'),
-    filename: '[name].[hash:5].js',
-    publicPath: '',
-  },
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	target: 'web',
+	// Don't attempt to continue if there are any errors.
+	bail: true,
+	devtool: '#eval-source-map',
+	entry: {
+		app: './demo/index.js',
+	},
+	output: {
+		path: path.join(__dirname, './build'),
+		filename: '[name].[hash:5].js',
+		publicPath: '',
+	},
 
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'demo/index.html',
-      filename: 'index.html',
-    })
-  ],
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'demo/index.html',
+			filename: 'index.html',
+		})
+	],
 
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    },
-    { test: /\.(scss|css)$/, loader: cssLoader },
-    {
-      test: /\.(png|jpg|gif|ico)$/,
-      loader: 'url-loader?limit=8192&name=assets/[name].[ext]'
-    }]
-  },
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/
+		},
+		{test: /\.(scss|css)$/, loader: cssLoader},
+		{
+			test: /\.(png|jpg|gif|ico)$/,
+			loader: 'url-loader?limit=8192&name=assets/[name].[ext]'
+		}]
+	},
 
-  devServer: {
-    port: port,
-    host: ip.address(),
-    contentBase: "./demo",
-    compress: true,
-    stats: 'minimal',
-  }
+	devServer: {
+		port: port,
+		host: ip.address(),
+		contentBase: "./demo",
+		compress: true,
+		stats: 'minimal',
+	}
 };
 
 if (isProd) {
-  config.devtool = '#source-map';
-  config.plugins.push(
-   
-    new ExtractTextPlugin('app.[hash:5].css', {allChunks: true})
-  );
+	config.devtool = '#source-map';
+	config.plugins.push(
+
+		new ExtractTextPlugin('app.[hash:5].css', {allChunks: true})
+	);
 }
 
 module.exports = config;
